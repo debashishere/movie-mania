25dee1f6b069e5d15cefe63ddba7f855e031cc38
debashishere <debashishere007@gmail.com>
2021-02-13 23:51:22 +0530

    login footer style fixed

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -2,7 +2,7 @@ const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-container');
 const videoContainer = document.querySelector('.video-container');
 const modalClose = document.getElementById('modal-close');
-const baseUrl = `https://fav-movie-mania.herokuapp.com`
+const baseUrl = `http://localhost:3000`
 
 
 const logoutBtn = document.getElementById('logout-btn');
