41c042532fc6857758878f6a629b5f9adb3c49f0
debashishere <debashishere007@gmail.com>
2021-02-13 13:29:19 +0530

    conflicts resolved

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-=======
-
->>>>>>> secureapi
 const api_key = `86f9a380ffae3c355aa19faaf111a15a`
 const baseUrl = `https://api.themoviedb.org/3`
 const axios = require('axios');
@@ -49,10 +45,6 @@ const getReviews = async (url) => {
 
     try {
         const result = await axios.get(url);
-<<<<<<< HEAD
-        console.log('res', result.data);
-=======
->>>>>>> secureapi
         return result.data
     }
 
@@ -97,10 +89,6 @@ router.get('/movie/now_playing', async (req, res) => {
 router.get('/search/movie', async (req, res) => {
 
     try {
-<<<<<<< HEAD
-        console.log('here')
-=======
->>>>>>> secureapi
         const term = req.query.term;
         const path = '/search/movie'
         const url = generateUrl(path) + '&query=' + term;
@@ -123,10 +111,6 @@ router.get('/movie/upcoming', async (req, res) => {
     try {
         const path = `/movie/upcoming`
         const url = generateUrl(path);
-<<<<<<< HEAD
-        console.log('url', url)
-=======
->>>>>>> secureapi
         const resData = await requestMovie(url);
         const movies = resData.results
         res.status(200).send(movies);
@@ -146,10 +130,6 @@ router.get('/movie/top_rated', async (req, res) => {
     try {
         const path = `/movie/top_rated`
         const url = generateUrl(path);
-<<<<<<< HEAD
-        console.log('url', url)
-=======
->>>>>>> secureapi
         const resData = await requestMovie(url);
         const movies = resData.results
         res.status(200).send(movies);
@@ -169,10 +149,6 @@ router.get('/movie/popular', async (req, res) => {
     try {
         const path = `/movie/popular`
         const url = generateUrl(path);
-<<<<<<< HEAD
-        console.log('url', url)
-=======
->>>>>>> secureapi
         const resData = await requestMovie(url);
         const movies = resData.results
         res.status(200).send(movies);
