1fff0b1f2d1669ad1288ec3c04b563387ab0c425
debashishere <debashishere007@gmail.com>
2021-02-13 23:30:12 +0530

    session store option edited

--- config/session-config.js
+++ config/session-config.js
@@ -5,6 +5,7 @@ const connection = require('./database-config');
 
 var options = {
     host: process.env.HOST,
+    port: 3306,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
